91bebc99d7ab9302c555d068640f966ca11e03f6
NikoSalchak <nikosalchak@gmail.com>
2024-07-24 20:54:01 +0700

    3. GC. Out Of Mem. Тестовое задание. [#504790]

---
+++ src/main/java/ru/job4j/articles/model/Article.java
@@ -0,0 +1,60 @@
+package ru.job4j.articles.model;
+
+import java.util.Objects;
+
+public class Article {
+
+    private int id;
+
+    private String text;
+
+    public Article(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public Article(String text) {
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Article article = (Article) o;
+        return id == article.id && Objects.equals(text, article.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{"
+                + "id=" + id
+                + ", text='" + text + '\''
+                + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/articles/model/Word.java
@@ -0,0 +1,56 @@
+package ru.job4j.articles.model;
+
+import java.util.Objects;
+
+public class Word {
+
+    private int id;
+
+    private String value;
+
+    public Word(int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Word word = (Word) o;
+        return id == word.id && Objects.equals(value, word.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Word{"
+                + "id=" + id
+                + ", value='" + value + '\''
+                + '}';
+    }
+}
